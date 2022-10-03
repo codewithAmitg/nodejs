cc13e3e0a4195ce4122f68c9f16205abd0ff76eb
codewithamitg <codewithamitgupta@gmail.com>
2022-10-03 10:22:07 +0545

    crud opeartion for file system

--- index.js
+++ index.js
@@ -1,47 +1,8 @@
-// const http = require('http');
-// const data = require('./data');
+//Asynchronous programming
+console.log("start exe ...");
 
-// http.createServer((req,res)=>{
-//     res.writeHead(200,{'Content-Type':'application\json'});
-//     res.write(JSON.stringify(data));
-//     res.end();
-// }).listen(4500);
-// console.log(process.argv[2]);
-const fs=require('fs');
-const path = require('path');
-const dirPath = path.join(__dirname,'files');
-const filePath =`${dirPath}/hello0.txt`;
+setTimeout(()=>{
+    console.log("logic exe...");
+},2000);
 
-// print dynamic files
-/* for(i=0;i<5;i++)
-{
-    fs.writeFileSync(dirPath +"/hello"+i+".txt",'a simple text');
-} */
-
-//read dir
-/* fs.readdir(dirPath,(err,files)=>{
-
-    files.forEach((item)=>{
-        console.log(item);
-    });
-}); */
-
-//read file
-/* fs.readFile(filePath,'utf8',(err,item)=>{
-    console.log(item);
-}); */
-
-//append file
-/* fs.appendFile(filePath,'This is append text',(err)=>{
-    if(!err) console.log('file is updated');
-}); */
-
-//rename file
-/* fs.rename(filePath,`${dirPath}/hello.txt`,(err)=>{
-    if(!err)console.log("file name is updated");
-}); */
-
-//delete file
-/* fs.unlinkSync(`${dirPath}/hello.txt`,(err)=>{
-    if(!err)console.log('file is deleted');
-}); */
+console.log("complete exe..");
